0ebd62217289a978783bb968a7695b7b7cfe34d1
Charu Yasas <charuyasas@gmail.com>
2023-10-19 17:25:27 +0530

    Renamed commandData variable

--- app/UseCases/StoreWebsitePostUseCase.php
+++ app/UseCases/StoreWebsitePostUseCase.php
@@ -8,12 +8,12 @@ use App\Models\User;
 
 class StoreWebsitePostUseCase
 {
-    public function execute(User $user, CreatePostCommand $commandData)
+    public function execute(User $user, CreatePostCommand $command)
     {
         $posts = new Post();
-        $posts->website_id = $commandData->websiteId;
-        $posts->title = $commandData->title;
-        $posts->description = $commandData->description;
+        $posts->website_id = $command->websiteId;
+        $posts->title = $command->title;
+        $posts->description = $command->description;
         $posts->user_id = $user->id;
         $posts->save();
         return $posts;
